0a72e0ee725f18fa1ba99c02db4f81a4ff10bc09
MD. Sujad UD <sujad.ud@outlook.com>
2024-02-04 17:43:52 +0600

    Task Completed lists: * Added Send message using HttpClient Post For sms.net.bd. * Added SMS Report method HttpClient Get Json Return. * Added Current balance chacking method using SMS Api. * Deployment NuGet package & Testing all functionalities.

--- SMS_Net_BD/SmsSender.cs
+++ SMS_Net_BD/SmsSender.cs
@@ -10,50 +10,169 @@ namespace SMS_Net_BD
 {
     public class SmsSender
     {
-        public static async Task<string> SendSMS(string API_Key, string To_Phone_Number, string Text_Massage)
+        //Private string variable
+        private string _apiKey = string.Empty;
+
+        /// <summary>
+        /// Default Constructor with a Param
+        /// </summary>
+        /// <param name="apiKey"></param>
+        public SmsSender(string apiKey)
+        {
+            _apiKey = apiKey;
+        }
+
+        HttpClient client = new HttpClient();
+
+        /// <summary>
+        /// Send SMS using sms.net.bd API
+        /// </summary>
+        /// <param name="To_Phone_Number"></param>
+        /// <param name="Text_Massage"></param>
+        /// <returns></returns>
+        /// <exception cref="SmsException"></exception>
+        public async Task<string> SendSMS(string To_Phone_Number, string Text_Massage)
         {
             try
             {
-                using var client = new HttpClient();
+                //using var client = new HttpClient();
 
                 string API_Url = "https://api.sms.net.bd/sendsms";
 
                 client.BaseAddress = new Uri(API_Url);
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-                var response = await client.GetAsync($"?api_key={API_Key}&msg={Text_Massage}&to={To_Phone_Number}");
+                var requestData = new
+                {
+                    api_key = _apiKey,
+                    msg = Text_Massage,
+                    to = To_Phone_Number
+                };
+
+                var content = new StringContent(JsonConvert.SerializeObject(requestData), Encoding.UTF8, "application/json");
+
+                var response = await client.PostAsync(API_Url, content);
 
                 if (response.IsSuccessStatusCode)
                 {
-                    var content = await response.Content.ReadAsStringAsync();
-                    dynamic stuff = JsonConvert.DeserializeObject(content);
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    dynamic result = JsonConvert.DeserializeObject(responseContent);
 
-                    if (stuff.error == "0")
+                    if (result.error == "0")
                     {
-                        string a = (string)stuff.data;
-                        return a;
+                        return responseContent;
                     }
                     else
                     {
-                        throw new SmsSendingException($"Sms Not Send, {stuff.msg}");
+                        return $"Error: {result.error}, {result.msg}";
                     }
                 }
                 else
                 {
-                    throw new SmsSendingException($"Error: {response.StatusCode}");
+                    throw new SmsException($"Error: {response.StatusCode}");
                 }
             }
             catch (Exception ex)
             {
-                throw new SmsSendingException($"An error occurred: {ex.Message}", ex);
+                throw new SmsException($"An error occurred: {ex.Message}", ex);
+            }
+        }
+
+        /// <summary>
+        /// Get SMS Report by Id using sms.net.bd API
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        /// <exception cref="SmsException"></exception>
+        public async Task<string> ReportSMS(int Id)
+        {
+            try
+            {
+                using var client = new HttpClient();
+
+                string API_Url = $"https://api.sms.net.bd/report/request/{Id}/";
+
+                client.BaseAddress = new Uri(API_Url);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.GetAsync($"?api_key={_apiKey}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    dynamic result = JsonConvert.DeserializeObject(responseContent);
+
+                    if (result.error == "0")
+                    {
+                        return responseContent;
+                    }
+                    else
+                    {
+                        throw new SmsException($"Error: {result.error}, {result.msg}");
+                    }
+                }
+                else
+                {
+                    throw new SmsException($"Error: {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new SmsException($"An error occurred: {ex.Message}", ex);
+            }
+        }
+
+        /// <summary>
+        /// Check your Current Balance using sms.net.bd API
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="SmsException"></exception>
+        public async Task<string> Balance()
+        {
+            try
+            {
+                using var client = new HttpClient();
+
+                string API_Url = "https://api.sms.net.bd/user/balance/";
+
+                client.BaseAddress = new Uri(API_Url);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.GetAsync($"?api_key={_apiKey}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    dynamic result = JsonConvert.DeserializeObject(responseContent);
+
+                    if (result.error == "0")
+                    {
+                        return responseContent;
+                    }
+                    else
+                    {
+                        throw new SmsException($"Error: {result.error}, {result.msg}");
+                    }
+                }
+                else
+                {
+                    throw new SmsException($"Error: {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new SmsException($"An error occurred: {ex.Message}", ex);
             }
         }
     }
 
-    public class SmsSendingException : Exception
+    /// <summary>
+    /// SMS Exception
+    /// </summary>
+    public class SmsException : Exception
     {
-        public SmsSendingException(string message) : base(message) { }
+        public SmsException(string message) : base(message) { }
 
-        public SmsSendingException(string message, Exception innerException) : base(message, innerException) { }
+        public SmsException(string message, Exception innerException) : base(message, innerException) { }
     }
 }
